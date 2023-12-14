5aebfe07529cb3ae36e81fb5458e5fcc657d7d83
RAMAKRISHNAKARAMPURI <karampuriramakrishnak@gmail.com>
2023-12-14 20:45:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const cmd = 'ps';
+let cmd = 'ps';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
