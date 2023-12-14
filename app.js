d61ad53d495f928afc7b686993b9385b76f49d5e
RAMAKRISHNAKARAMPURI <karampuriramakrishnak@gmail.com>
2023-12-14 19:36:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,49 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post('/slack/command', async (req, res) => {
+  console.log('testing');
+  const { command, text, response_url, user_id, channel_id } = req.body;
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+  // Make a request to your server to get the details
+  const details = await fetchDetailsFromServer();
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  // Construct the message
+  const message = `Details from the server: ${details}`;
+
+  // Respond to the Slack command
+  await sendResponseToSlack(response_url, message);
+
+  res.status(200).end();
+});
+
+async function fetchDetailsFromServer() {
+  // Implement logic to fetch details from your server
+  // ...
+
+  // For demonstration purposes, let's assume a simple response
+  return 'Server details go here';
+}
+
+async function sendResponseToSlack(responseUrl, message) {
+  // Send the constructed message back to Slack
+  await axios.post(responseUrl, {
+    text: message,
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+// npm init -y
+// npm install express body-parser axios
+// node app.js
